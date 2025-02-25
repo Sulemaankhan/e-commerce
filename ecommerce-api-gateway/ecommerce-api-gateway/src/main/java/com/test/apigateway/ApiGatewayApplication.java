f70ca04486d5de8471a4076d854df6f3702fe9b7
khan <sulemaankhan9595@gmail.com>
2025-02-25 12:25:31 +0530

    update the application name

--- ecommerce-api-gateway/ecommerce-api-gateway/src/main/java/com/test/apigateway/ApiGatewayApplication.java
+++ ecommerce-api-gateway/ecommerce-api-gateway/src/main/java/com/test/apigateway/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EcommerceApiGatewayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApiGatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
